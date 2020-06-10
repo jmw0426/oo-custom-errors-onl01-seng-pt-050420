8633e243fcd5ee056fd2aa25de04e1f007b4f30d
Joshua Williams <jmw0426@hotmail.com>
2020-06-10 03:30:41 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,11 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
-  end
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
 
 end
 
